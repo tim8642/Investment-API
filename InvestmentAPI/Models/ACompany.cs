17a174904111cede8c393a0179d62e3f002ce722
tim8642 <59577781+tim8642@users.noreply.github.com>
2020-03-08 22:18:07 +0000

    first push

---
+++ InvestmentAPI/Models/ACompany.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InvestmentAPI.Models
+{
+    public class ACompany
+    {
+    }
+}
---
+++ InvestmentAPI/Models/StockBrokerDbContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace InvestmentAPI.Models
+{
+    public class StockBrokerDbContext: DbContext
+    {
+        public StockBrokerDbContext() { }
+        public StockBrokerDbContext(DbContextOptions<StockBrokerDbContext> options) : base(options) { }
+
+        public DbSet<StockBroker> stockBrokers { get; set; }
+        public DbSet <Customer> customers { get; set; }
+        public DbSet <ACompany> aCompanies { get; set; }
+        public DbSet<BCompany> bCompanies { get; set; }
+        public DbSet<CCompany> cCompanies { get; set; }
+
+
+
+        string connectionString = @"Data Source = (localdb)\MSSQLLocalDB;Initial Catalog = InvestmentDatabase;";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.UseSqlServer(connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+
+        }
+    }
+}
